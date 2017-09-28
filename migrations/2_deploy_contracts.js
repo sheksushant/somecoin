7778bb3f55eb3640844616dfa3e2e46ddcdbdb92
Sushant Shekhar <sushantshekhar20@gmail.com>
2017-09-28 07:19:11 +0530

    contract added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const GustavoCoinCrowdsale = artifacts.require("./GustavoCoinCrowdsale.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function(deployer, network, accounts) {
+  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
+  const endTime = startTime + (86400 * 20) // 20 days
+  const rate = new web3.BigNumber(1000)
+  const wallet = accounts[0]
+
+  deployer.deploy(GustavoCoinCrowdsale, startTime, endTime, rate, wallet)
 };
